56c488e2ba5de7e6989a6a356122f24814972913
RenatoFloreon <renato@floreon.app.br>
2025-05-18 19:23:03 -0300

    v12 agora vai?

--- api/webhook.js
+++ api/webhook.js
@@ -1,26 +1,10 @@
-// api/webhook.js
-export default function handler(req, res) {
-  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+const { createServer } = require('http');
+const app = require('../lib/server');
 
-  if (req.method === 'GET') {
-    const mode = req.query['hub.mode'];
-    const token = req.query['hub.verify_token'];
-    const challenge = req.query['hub.challenge'];
-
-    if (mode && token && mode === 'subscribe' && token === VERIFY_TOKEN) {
-      console.log('[WEBHOOK] Verificado com sucesso!');
-      return res.status(200).send(challenge);
-    } else {
-      console.warn('[WEBHOOK] Falha na verificação.');
-      return res.sendStatus(403);
-    }
+module.exports = (req, res) => {
+  if (req.url.startsWith('/api/webhook')) {
+    req.url = req.url.replace('/api', ''); // Corrige rota para Express
   }
-
-  if (req.method === 'POST') {
-    const body = req.body;
-    console.log('[WEBHOOK] Evento recebido:', JSON.stringify(body, null, 2));
-    return res.status(200).send('EVENT_RECEIVED');
-  }
-
-  res.status(404).send('Método não suportado');
-}
+  const server = createServer(app);
+  server.emit('request', req, res);
+};
