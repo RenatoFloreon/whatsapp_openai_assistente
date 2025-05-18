d9d153d0d5c6ba96378d5f785da796941d142a53
RenatoFloreon <renato@floreon.app.br>
2025-05-18 18:43:04 -0300

    v11 oremos

--- webhook.js
+++ webhook.js
@@ -1,23 +1,25 @@
 module.exports = (req, res) => {
-  if (req.method === 'GET') {
-    const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
+  if (req.method === 'GET') {
     const mode = req.query['hub.mode'];
     const token = req.query['hub.verify_token'];
     const challenge = req.query['hub.challenge'];
 
     if (mode && token && mode === 'subscribe' && token === VERIFY_TOKEN) {
       console.log('[WEBHOOK] Verificado com sucesso!');
-      res.status(200).send(challenge);
+      return res.status(200).send(challenge);
     } else {
       console.warn('[WEBHOOK] Falha na verificação.');
-      res.sendStatus(403);
+      return res.sendStatus(403);
     }
   }
 
   if (req.method === 'POST') {
     const body = req.body;
     console.log('[WEBHOOK] Evento recebido:', JSON.stringify(body, null, 2));
-    res.status(200).send('EVENT_RECEIVED');
+    return res.status(200).send('EVENT_RECEIVED');
   }
+
+  res.status(404).send('Método não suportado');
 };
